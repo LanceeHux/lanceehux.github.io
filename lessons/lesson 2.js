63368c1e3fee928948378068ad021f0ac19c8583
LeeHux <laziolynx@gmail.com>
2026-06-18 15:53:02 +0800

    Add lesson 2 with player attack functionality

---
+++ lessons/lesson 2.js
@@ -0,0 +1,12 @@
+// lesson 2
+let Player = {
+    name: "Lanx",
+    health: 100
+}
+function attack(user) {
+    user.health -= 20;
+    console.log(`${user.name} got attacked! health: ${user.health}`);
+}
+for(let i = 0;i < 3;i++) {
+  attack(Player);
+}// lesson end [2]
