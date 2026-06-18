5a580a3a66858c4ad889eea98b05bb6a0a8f3307
LeeHux <laziolynx@gmail.com>
2026-06-18 15:52:16 +0800

    lesson 1, self taught

---
+++ lessons/lesson 1.js
@@ -0,0 +1,15 @@
+// lesson 1
+const player = {
+    name: "Lanx",
+    level: 3,
+    inventory: ["Sword", "Potion"]
+}
+// Display the prompt popup and store the user's text in a variable
+function game() {
+    player.level += 2;
+    player.inventory.push("Shield");
+
+    console.log(JSON.stringify(player));
+}
+game();
+//lesson end [1]
