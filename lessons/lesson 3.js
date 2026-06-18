0b76b4835a4c1b5c7e620933091499d36f5dc0bf
LeeHux <laziolynx@gmail.com>
2026-06-18 16:09:56 +0800

    Add lesson 3 with player attack functionality

---
+++ lessons/lesson 3.js
@@ -0,0 +1,16 @@
+// lesson 3
+const players = [
+    {name: "Lanx", hp: 100},
+    {name: "Rico", hp: 100},
+    {name: "Ana", hp: 100}
+];
+
+function attackAll(user) {
+    for(let i = 0; i < user.length;i++) {
+        user[i].hp -= 10;
+        console.log(`${user[i].name} ${user[i].hp}`);
+    }
+}
+
+attackAll(players);
+// lesson end [3]
