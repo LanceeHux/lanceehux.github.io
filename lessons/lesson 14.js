6d62aa3c67e7bf17d6ee0aa11e4fcad5869af5a9
LeeHux <laziolynx@gmail.com>
2026-06-21 20:03:26 +0800

    Implement character classes and game mechanics
    learned: polymorphism with each classes with same name function but different object caling

---
+++ lessons/lesson 14.js
@@ -0,0 +1,50 @@
+// lesson 14
+class Character {
+    #hp = 100;
+    constructor(name) {
+        this.name = name;
+    }
+    attack(target) {
+         let dmg = 10;
+         console.log(`${this.name} attacks ${target.name}, with a ${dmg} damage!`);
+         target.damage(dmg);
+    } 
+    damage(dmg) {
+        this.#hp -= dmg;
+        console.log(`${this.name} received a ${dmg} damage! HP: ${this.#hp}`);
+    }
+    show () {
+        console.log( { name: this.name, hp: this.#hp } );
+    }
+}
+class Mage extends Character {
+    attack(target) {
+        let dmg = 40;
+        console.log(`${this.name} casts a ${dmg} damage spell to ${target.name}! `);
+        target.damage(dmg);
+    }
+}
+class Warrior extends Character {
+    attack(target) {
+        let dmg = 20;
+        console.log(`${this.name} swing their sword on ${target.name}, and casted a ${dmg} damage!`);
+        target.damage(dmg);
+    }
+}
+
+const Lanx = new Mage("Lanx");
+const Rico = new Warrior("Rico");
+const Ana = new Character("Ana");
+
+function gameUpdate(x) {
+    let action = x.action;
+    x.char[action](x.target, x.value);
+}
+gameUpdate( { char: Lanx, action: "attack", target: Ana } );
+gameUpdate( { char: Rico, action: "attack", target: Ana } );
+gameUpdate( { char: Ana, action: "attack", target: Lanx } );
+gameUpdate( { char: Lanx, action: "show" } );
+gameUpdate( { char: Rico, action: "show" } );
+gameUpdate( { char: Ana, action: "show" } );
+
+// lesson end [14]
